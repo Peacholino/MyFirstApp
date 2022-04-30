b43300f02f2d9858c58e2867168e3e11ec0fb034
Peacholino <pia.e.schuster@web.de>
2022-04-30 21:38:55 +0200

    My first Button

--- app/src/main/java/com/example/myfirstapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/myfirstapp/ui/main/MainFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import com.example.myfirstapp.R
 
@@ -24,10 +25,16 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    override fun onViewCreated (view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val textfield = view.findViewById<TextView>(R.id.message)
-        textfield.text = "My own Text"
+        val textfield = view.findViewById<TextView>(R.id.message)           // Adds a value to call the text field
+        textfield.text = "My own Text"                                     // Changes the text of the text field
+
+        val myButton = view.findViewById<Button>(R.id.btn_first)          // Adds Button as value
+        myButton.text = "Click me!"                                       // Adds text to button
+        myButton.setOnClickListener {
+            myButton.text = "I was clicked"                              // Makes it so the text changes when clicked
+        }
 }
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
